e61ae6d134803a031f1bbec9223cfa2e5640168e
Odise Vila <odisevila@odises-macbook-pro.local>
2018-12-14 15:28:24 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Odise Vila on 14/12/2018.
+//  Copyright © 2018 Odise Vila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
