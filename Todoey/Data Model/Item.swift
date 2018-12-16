a136bf6ecf0ef2e227a772e86feb685b07bb918c
Odise Vila <odisevila@odises-macbook-pro.local>
2018-12-16 16:41:10 +0100

    Redesign to MVC pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Odise Vila on 16/12/2018.
+//  Copyright © 2018 Odise Vila. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
